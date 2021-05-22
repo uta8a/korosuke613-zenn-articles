2694d88bcbb7b5628314779f209de8162dfc082c
Futa Hirakoba <futa-hirakoba@cybozu.co.jp>
2021-05-22 13:36:03 +0900

    fix: the problem of erasing the number 0 (ex: 05 -> 5)

--- articles/template/generate-productivity-weekly-template.sh
+++ articles/template/generate-productivity-weekly-template.sh
@@ -5,13 +5,17 @@ set -euxo pipefail
 source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # ProductivityWeekly何回目かを計算する
-next_count=$(($(cat articles/template/productivity-weekly-count.txt) + 1))
+export PW_COUNT=$(($(cat articles/template/productivity-weekly-count.txt) + 1))
+
+export PW_YEAR=$1
+export PW_MONTH=$2
+export PW_DAY=$3
 
 # テンプレから本文を出力する
-gotemplate --var count=$next_count --var year=$1 --var month="$2" --var day=$3 -P $source_dir/productivity-weekly-template.md.tmpl > "$source_dir/../productivity-weekly-20$1$2$3.md"
+gomplate -f ./articles/template/productivity-weekly-template.md.tmpl > "$source_dir/../productivity-weekly-20$1$2$3.md"
 
 # カウントを更新する
-echo $next_count > $source_dir/productivity-weekly-count.txt
+echo $PW_COUNT > $source_dir/productivity-weekly-count.txt
 
 # ブランチ生成
 git switch -c "pw-20$1$2$3"
