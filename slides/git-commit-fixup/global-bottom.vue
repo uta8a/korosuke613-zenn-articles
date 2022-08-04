9498a8a6f03791b8619dfd4f759f11f3e4311ab2
Futa HIRAKOBA <korosuke613613@gmail.com>
2022-08-04 17:52:23 +0900

    fix: git commit fixup

--- slides/git-commit-fixup/global-bottom.vue
+++ slides/git-commit-fixup/global-bottom.vue
@@ -5,7 +5,7 @@
     class="text-gray-400 absolute bottom-0 left-max right-1 p-1"
     style="font-size: 0.7rem;"
   >
-    <span style="font-size: 0.5em">ぼくのかんがえたさいきょうの開発環境 天下一武道会</span> -
+    <span>ぼくのかんがえたさいきょうの開発環境 天下一武道会</span> -
     [ {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }} ]
   </footer>
 </template>
