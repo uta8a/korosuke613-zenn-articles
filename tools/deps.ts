4eb3fe6c9f5a77652f0ea1aca67cf84f2b9faf7f
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-08-29 14:49:14 +0900

    feat: support Structured Outputs by OpenAI API
    https://platform.openai.com/docs/guides/structured-outputs

--- tools/deps.ts
+++ tools/deps.ts
@@ -3,11 +3,11 @@ export { matter };
 export { marked } from "npm:marked@6.0.0";
 export type { Token, Tokens } from "npm:marked@6.0.0";
 
-export { OpenAI } from "https://deno.land/x/openai@v4.47.1/mod.ts";
+export { OpenAI } from "https://deno.land/x/openai@v4.56.0/mod.ts";
 export type {
   ChatCompletionCreateParamsNonStreaming,
   CompletionUsage,
-} from "https://deno.land/x/openai@v4.47.1/resources/mod.ts";
+} from "https://deno.land/x/openai@v4.56.0/resources/mod.ts";
 
 export { join } from "https://deno.land/std@0.224.0/path/join.ts";
 export { LogRecord } from "https://deno.land/std@0.224.0/log/mod.ts";
